58576e302999123b46aec5af4ba208ccf55f697f
Marc Plarisan <plarisanmarc@gmail.com>
2022-11-26 01:17:24 +0800

    Updated startup.sh

--- Scripts/startup.sh
+++ Scripts/startup.sh
@@ -2,9 +2,18 @@ printf "\nWelcome back, great DragunWF of the Philippines...\n\n"
 printf "Commands:\n- move-img\n- switch-dir <index>\n\n"
 username="dragunwf"
 
+function dir {
+    ls -l
+}
+
 function move-img {
     echo "Running script..."
     python3 "/Users/$username/documents/devstuff/repositories/mini-scripts/personal/change_image_location.py"
+}
+
+function delete-img {
+    echo "Running delete script..."
+    python3 ""
 }
 
 function switch-dir () {
